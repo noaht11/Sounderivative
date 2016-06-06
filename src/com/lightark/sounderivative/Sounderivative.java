10fab774dec8f76e6438abec851c7623910e7841
Noah Tajwar <noah.tajwar.oss@gmail.com>
2016-06-06 11:39:29 -0700

    Added support for integrating a WAV file
    Works for undoing a derivative with resepct to sample number, but not for with
    respect to time

--- src/com/lightark/sounderivative/Sounderivative.java
+++ src/com/lightark/sounderivative/Sounderivative.java
@@ -6,19 +6,25 @@ import java.util.Arrays;
 
 public class Sounderivative
 {
-    private static final boolean DEBUG = false;
+    private static final int MODE_DERIVATIVE = 1;
+    private static final int MODE_INTEGRAL = 2;
 
     private static final int RESPECT_TIME = 1;
     private static final int RESPECT_SAMPLE_NUMBER = 2;
 
+    // Config
+    private static final boolean DEBUG = false;
+    private static int MODE;
     private static final int WITH_RESPECT_TO = RESPECT_SAMPLE_NUMBER;
 
     public static void main(String[] args)
     {
+        MODE = Integer.parseInt(args[0]);
+
         try
         {
-            File inputFile = new File(args[0]);
-            File outputFile = new File(args[1]);
+            File inputFile = new File(args[1]);
+            File outputFile = new File(args[2]);
 
             // Prepare the input file
             WavFile inputWavFile = WavFile.openWavFile(inputFile);
@@ -33,18 +39,23 @@ public class Sounderivative
             int framesRead;
 
             // Prepare the output file
-            WavFile outputWavFile = WavFile.newWavFile(outputFile, numChannels, numFrames, validBits, sampleRate);
+            WavFile outputWavFile = WavFile.newWavFile(outputFile, numChannels, numFrames - 1, validBits, sampleRate);
             double[][] outputBuffer = new double[numChannels][100];
 
             System.out.println("Beginning Processing...");
             int iterationCount = 0;
 
+            double deltaT = 1.0 / sampleRate;
+
             double[] previousSamples = new double[numChannels];
             Arrays.fill(previousSamples, Double.NaN);
-            double deltaT = 1.0 / sampleRate;
+
+            double accumulatedArea = 0;
+            int totalOutputFrameCount = 0;
             do
             {
                 System.out.println("Processing frames: " + iterationCount*100 + "-" + (iterationCount+1)*100);
+                System.out.println("");
 
                 // Read frames into buffer
                 framesRead = inputWavFile.readFrames(inputBuffer, 100);
@@ -65,24 +76,41 @@ public class Sounderivative
                             {
                                 case RESPECT_TIME:
                                     dx = deltaT;
+                                    break;
                                 case RESPECT_SAMPLE_NUMBER:
                                 default:
                                     dx = 1.0;
+                                    break;
                             }
 
-                            double derivative = (sample - previousSample) / dx;
-                            outputBuffer[c][outputFrameCount] = derivative;
-                            outputWritten = true;
+                            double dy = sample - previousSample;
+
+                            double outputSample = sample;
+                            switch(MODE)
+                            {
+                                case MODE_DERIVATIVE:
+                                    outputSample = dy / dx;
+                                    break;
+                                case MODE_INTEGRAL:
+                                    accumulatedArea += dy * dx;
+                                    outputSample = accumulatedArea;
+                                    break;
+                            }
 
                             if(DEBUG)
                             {
                                 System.out.println("Previous sample: " + new BigDecimal(previousSample).toPlainString());
                                 System.out.println("Sample: " + new BigDecimal(sample).toPlainString());
-                                System.out.println("Delta T: " + new BigDecimal(deltaT).toPlainString());
-                                System.out.println("Derivative: " + new BigDecimal(derivative).toPlainString());
+                                System.out.println("dx: " + new BigDecimal(dx).toPlainString());
+                                System.out.println("dy: " + new BigDecimal(dy).toPlainString());
+                                System.out.println("Output sample: " + new BigDecimal(outputSample).toPlainString());
                                 System.out.println("");
                             }
+
+                            outputBuffer[c][outputFrameCount] = outputSample;
+                            outputWritten = true;
                         }
+
                         previousSamples[c] = sample;
                     }
 
@@ -93,6 +121,7 @@ public class Sounderivative
                 }
 
                 outputWavFile.writeFrames(outputBuffer, outputFrameCount);
+                totalOutputFrameCount += outputFrameCount;
                 iterationCount++;
 
                 if(DEBUG)
@@ -102,6 +131,9 @@ public class Sounderivative
             }
             while(framesRead != 0);
             System.out.println("Processing complete");
+
+            System.out.println("Frames read: " + numFrames);
+            System.out.println("Frames written: " + totalOutputFrameCount);
 
             // Close the files
             inputWavFile.close();
